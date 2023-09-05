0c80dbe01891bc7875d31190905c1d6e172dcebd
git-elliot <fs0c19ty@protonmail.com>
2023-09-05 10:25:29 +0530

    removed arp test

--- test/network_tools_flutter_test.dart
+++ test/network_tools_flutter_test.dart
@@ -1,12 +1,10 @@
-import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('adds one to input values', () {});
-  test('arp scan ', () async {
-    ProcessResult result = await Process.run('arp', ['-a']);
-    print(result.stdout);
-    print("ARP scan ended");
-  });
+  // test('arp scan ', () async {
+  //   ProcessResult result = await Process.run('arp', ['-a']);
+  //   print(result.stdout);
+  //   print("ARP scan ended");
+  // });
 }
