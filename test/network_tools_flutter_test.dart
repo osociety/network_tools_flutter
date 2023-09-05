6d4094882f82f6ddaa6e6d9760f67f05f3f7f97b
git-elliot <fs0c19ty@protonmail.com>
2023-09-05 09:53:43 +0530

    add code to test

--- test/network_tools_flutter_test.dart
+++ test/network_tools_flutter_test.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  test('adds one to input values', () async {
+    ProcessResult result = await Process.run('arp', ['-a']);
+    print(result.stdout);
+    print("ARP scan ended");
+  });
 }
