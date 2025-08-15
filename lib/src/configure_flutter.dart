da10dcc2a2e92217f472f8605ec8890dc2dc52b0
git-elliot <fs0c19ty@protonmail.com>
2025-08-15 18:05:43 +0530

    fixed errors

--- lib/src/configure_flutter.dart
+++ lib/src/configure_flutter.dart
@@ -4,7 +4,7 @@ import 'package:network_tools/network_tools.dart' as packages_page;
 // ignore: implementation_imports
 import 'package:network_tools/src/services/arp_service.dart';
 // ignore: implementation_imports
-import 'package:network_tools/src/services/impls/arp_service_sembast_impl.dart';
+import 'package:network_tools/src/services/impls/arp_service_drift_impl.dart';
 // ignore: implementation_imports
 import 'package:network_tools_flutter/src/network_tools_flutter_util.dart';
 import 'package:network_tools_flutter/src/services_impls/host_scanner_service_flutter_impl.dart';
@@ -39,7 +39,7 @@ Future configureNetworkToolsFlutter(
   }
 
   // Setting dart native classes implementations
-  ARPServiceSembastImpl();
+  ARPServiceDriftImpl();
 
   // Setting flutter classes implementation
   HostScannerServiceFlutterImpl();
